3fc8cea59a2851bc1e08f3eaec1ab6b230f9a11f
Yamil Palmiero <yamilpalmiero@live.com>
2021-09-25 20:48:17 -0300

    Estructura de clases

---
+++ Palmiero.Yamil.PrimerParcial/Biblioteca/Computadora.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    public class Computadora
+    {
+        //ATRIBUTOS 
+        private string software;
+        private string periferico;
+        private string juego;
+        private string procesador;
+        private short ram;
+        private string placaVideo;
+
+        //CONSTRUCTOR
+        public Computadora(string software, string periferico, string juego, string procesador, short ram, string placaVideo)
+        {
+            this.software = software;
+            this.periferico = periferico;
+            this.juego = juego;
+            this.procesador = procesador;
+            this.ram = ram;
+            this.placaVideo = placaVideo;
+        }
+
+    }
+}
